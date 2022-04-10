89beaefd23dab16028d35704a415d883f8728063
nn98 <jkllhgb@gmail.com>
2022-04-11 00:02:34 +0900

    Lec1 warpping

---
+++ Another/path.js
@@ -0,0 +1,12 @@
+let a = ["c:/temp/guide.hwp",
+ "c:/pj/frontend/chap05/substring1.js",
+ "homework.docx"]
+
+let b="c:/pj/frontend/chap05/substring1.js";
+
+for(let i=0;i<a.length;console.log('extension : '+a[i++].match(/(?=\.).+/)[0]));
+
+for(let i=0;i<a.length;console.log('without extension : '+a[i++].match(/.+(?=\.)/)[0]));
+
+//const reg=new RegExp('\..*');
+//console.log(b.match(/(?<=\.).+/));
